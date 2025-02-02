5ada25534906d64ee31426e3f704a44365f33d09
Turzin <artur.cavalcante3107@gmail.com>
2025-02-02 18:12:03 -0300

    test 1

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,4 @@
 # How many new outputs were created by block 123,456?
+block=$(bitcoin-cli getblockhash 123456)
+total_out=$(bitcoin-cli getblock $block 2 | jq '[.tx[].vout | length] | add')
+echo $total_out
