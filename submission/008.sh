f539093e961698fa8bfbeaff637fa6a3ec5e5f4d
Turzin <artur.cavalcante3107@gmail.com>
2025-02-02 23:52:25 -0300

    test 8

--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,5 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+
+input=$(bitcoin-cli getrawtransaction "e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163" true| jq -r '.vin[0].txinwitness[2]')
+echo ${input:4:66} #OP_PUSHBYTES_33
